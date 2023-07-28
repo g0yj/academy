f6b570aa7b84d2d6c51fc938b61cf04873434a61
504 <504@DESKTOP-GR47Q4P>
2023-07-28 17:23:42 +0900

    sdfsdffsdfsdfsdf

--- 키오스크/src/키오스크/model/Dao.java
+++ 키오스크/src/키오스크/model/Dao.java
@@ -1,5 +1,9 @@
 package 키오스크.model;
 
-public class Dao {
+import 키오스크.model.Dto;
 
+
+public class Dao {
+   public static  drinkList = new Dto[];
+   public static  inventoryList = new Dto[100];
 }
--- 키오스크/src/키오스크/model/Dto.java
+++ 키오스크/src/키오스크/model/Dto.java
@@ -1,5 +1,68 @@
 package 키오스크.model;
 
 public class Dto {
+	private String name;
+	private int price;
+	private int stock;
+	private int basket;
+	
+	public Dto() {}
+	
 
+	public Dto(String name, int price, int stock, int basket) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.stock = stock;
+		this.basket = basket;
+		
+		
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+	public int getStock() {
+		return stock;
+	}
+
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+
+	public int getBasket() {
+		return basket;
+	}
+
+
+	public void setBasket(int basket) {
+		this.basket = basket;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Dto [name=" + name + ", price=" + price + ", stock=" + stock + ", basket=" + basket + "]";
+	}
+	
 }
