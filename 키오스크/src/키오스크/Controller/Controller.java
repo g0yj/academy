d86b6865af5db3bc4e7fdbd03481b6857af91a93
504 <504@DESKTOP-J4S8LEN>
2023-07-28 17:20:15 +0900

    ㅇㅇㅇㅇ

--- 키오스크/src/키오스크/Controller/Controller.java
+++ 키오스크/src/키오스크/Controller/Controller.java
@@ -1,5 +1,29 @@
 package 키오스크.Controller;
 
-public class Controller {
+import 키오스크.model.Dao;
+import 키오스크.model.Dto;
 
+public class Controller {
+	
+	private static Controller controller = new Controller();
+	
+	public static Controller getInstance() {return controller;}
+	
+	private Controller() {}
+
+//-----------------------------------------------------------------
+	public String inputNumLogic( int no ) {
+		
+		return Dao.drinkList[no].getName() + " 의 제품 "+Dao.drinkList[no].getPrice()+" 원 입니다.";
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- 키오스크/src/키오스크/view/View.java
+++ 키오스크/src/키오스크/view/View.java
@@ -1,5 +1,43 @@
 package 키오스크.view;
 
-public class View {
+import java.util.Scanner;
+
+import 키오스크.Controller.Controller;
+
+
+public class View {// 입출력 담당하는 클래스 // view -> HTML/JS
+	   // 0. 싱글톤 생성
+	   // 1. 현재 클래스로 에 static 객체 생성 , 필드는 무조건 잠금 
+	private static View view = new View();
+	   // 2. 외부 객체에서 해당 싱글톤 호출하기 위한 메소드 
+	public static View getInstance() { return view; }
+	   // 3. 객체 외부에서 해당 클래스로 객체 생성 잠금 , 생성자를 private 
+	private View() { }
+	
+	// 1. 필드
+	private Scanner sc = new Scanner(System.in); 
+	
+	// 2. 생성자
+	
+	// 3. 함수 
+	public void view() {
+	   while(true) { // 무한루프
+	      System.out.println("\n\n--------- 제품번호 ---------");
+	      System.out.print("1.음료 고르기");
+	      int ch = sc.nextInt();
+	      if( ch == 1 ) { inputNumber(); }
+	     
+	   } // while e 
+	} // f end 
+	
+	public void inputNumber() {
+		System.out.println("0.콜라 1.사이다 2.환타"); int no=sc.nextInt();
+		String result = Controller.getInstance().inputNumLogic(no);
+		System.out.println(result);
+	}
 
 }
+
+
+
+
