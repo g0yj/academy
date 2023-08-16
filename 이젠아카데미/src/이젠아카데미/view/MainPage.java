2a1d440a54617853d949681ea41ff61271bfacdb
504 <504@DESKTOP-FDBPKQC>
2023-08-16 15:51:08 +0900

    0816 15:51 기준 수정

--- 이젠아카데미/src/이젠아카데미/view/MainPage.java
+++ 이젠아카데미/src/이젠아카데미/view/MainPage.java
@@ -15,7 +15,6 @@ public class MainPage {
 			System.out.println("======================");
 			System.out.println("1. 직원 2. 학생");
 			System.out.println("======================");
-			// 여기에 3,4번 넣기(3.행정 4.원장) -> 기준
 			System.out.print("선택:  "); int ch= sc. nextInt();
 			
 			try { 
@@ -28,26 +27,9 @@ public class MainPage {
 						else if(select==4){}
 					}
 				else if(ch==2) {System.out.println(">>>>1.내정보 2.내가 쓴 글<<<<<");
-								//유효성 검사 필요. sql만들기
+								
 								System.out.print("회원이름입력: ");} 
-				/* else if(ch==3)(기준) {				
-				 1. 행정을 눌렀을 때 나와야 하는 출력값
-				 	syso 가눙 1 (직원등록), syso 기능 2 (직원퇴사처리?) , 
-				 	
-				 	1-1) 기능1만 할 경우
-				 	
-				 		System.out.print("직원등록: ");
-				 		StudentView.getStudentView().studentMain();//EmployeeView로 이동
-				 	
-				 	1-2) 기능1, 2 할 경우
-				 	 
-				 	 	System.out.print("선택: "); int select=sc.nextInt();
-						if(select==1) {StudentView.getStudentView().studentMain();}
-				 
-				 }
-				
-				
-				*/
+			
 			} catch (Exception e) {System.out.println("숫자로 입력하세요\n오류사유: "+e);
 				return ;
 			}	
